0d96eb692689302574a7d96cae110a64686ea026
Chad Jarreau <chad.jarreau@lifeomic.com>
2019-09-09 20:17:00 -0500

    basic news selector; aggregate news website

--- app.js
+++ app.js
@@ -4,18 +4,34 @@
 // everything endpoint: /v2/everything
 const apiKey = '717102b601a1474290e0afc6973c60a0';
 const main = document.querySelector('main');
+const sources = document.querySelector('#sources');
+const defaultSource = 'the-washington-post';
 
-window.addEventListener('load', e => {
+window.addEventListener('load', async e => {
     updateNews();
-    // updateSources();
+    await updateSources();
+
+    sources.value = defaultSource;
+
+    sources.addEventListener('change', e => {
+        updateNews(e.target.value);
+    })
 });
 
-async function updateNews() {
-    const newsUrl = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`;
+async function updateNews(source = defaultSource) {
+    const newsUrl = `https://newsapi.org/v2/top-headlines?sources=${source}&apiKey=${apiKey}`;
     const resp = await fetch(newsUrl);
     const json = await resp.json();
 
     main.innerHTML = json.articles.map(createArticle).join('\n');
+}
+
+async function updateSources() {
+    const sourcesUrl = `https://newsapi.org/v2/sources?apiKey=${apiKey}`;
+    const resp = await fetch(sourcesUrl);
+    const json = await resp.json();
+
+    sources.innerHTML = json.sources.map(source => `<option value="${source.id}">${source.name}</option>`).join('\n');
 }
 
 function createArticle(article) {
